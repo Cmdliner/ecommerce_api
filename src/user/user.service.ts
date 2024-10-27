e18392ddf0d5c5930910045b442422976125dd6f
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-10-27 19:28:12 +0100

    Error validation and throwing appropriate erros in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Model, Types } from 'mongoose';
 import { User, UserDocument } from './user.schema';
 import { InjectModel } from '@nestjs/mongoose';
@@ -8,14 +8,20 @@ import { hash } from 'bcryptjs';
 export class UserService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) { }
 
-    async profileInfo(id: Types.ObjectId): Promise<User> {
-        return this.userModel.findById(id)
+    async profileInfo(id: Types.ObjectId): Promise<UserDocument> {
+        return this.userModel.findById(id);
     }
-    async findOne(email: string) {
-        return this.userModel.findOne({ email });
+    async findOne(email: string): Promise<UserDocument> {
+        const user  = this.userModel.findOne({ email });
+        if(!user) throw new NotFoundException("User not found!");
+
+        return user
     }
 
-    async createUser(email: string, passsword: string): Promise<User> {
+    async createUser(email: string, passsword: string): Promise<UserDocument> {
+        const userExists = await this.userModel.findOne({ email });
+        if (userExists) throw new BadRequestException("User with that email already exists");
+
         const hashedPassword = await hash(passsword, 10);
         const user = this.userModel.create({ email, password: hashedPassword });
         return user;
