ca0fcbe3b685264504d6afaf5bed48d53e2ecb30
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-10-27 18:20:54 +0100

    Enabled cors; add url versioning and helmet for header security

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Use cors
+  app.enableCors();
+  app.use(helmet());
+
   // Add API URI versioning
-  app.setGlobalPrefix("api");
   app.enableVersioning({
     type: VersioningType.URI,
-    // prefix: "api"
-  })
+    prefix: "api/v",
+    defaultVersion: "1"
+  });
   await app.listen(3000);
 }
 bootstrap();
