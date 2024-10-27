4030a47ab177333c4899b4724e8cf6b63e394579
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-10-27 19:26:58 +0100

    Rm try-catch and defaults to nest eerror handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { CreateUserDto, UserLoginDto } from 'src/dtos/create-user.dto';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 
-@Controller({ path: 'auth', version: '1' })
+@Controller('auth')
 export class AuthController {
 
     constructor(private userService: UserService, private authService: AuthService) { }
@@ -11,22 +11,15 @@ export class AuthController {
     @Post('register')
     @HttpCode(201)
     async register(@Body() body: CreateUserDto) {
-        try {
             const user = await this.userService.createUser(body.email, body.password);
             return { success: true, user };
-        } catch (error) {
-            console.error(error);
-        }
     }
 
     @Post('login')
     @HttpCode(200)
     async login(@Body() body: UserLoginDto) {
-        try {
             const { access_token } = await this.authService.signIn(body.email, body.password);
             return { success: true, access_token }
-        } catch (error) {
-            console.error(error);
-        }
     }
+
 }
